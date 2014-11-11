d4991408f5855acedca3daf162061a7dc3627179
Christophe <christopho128@gmail.com>
2014-11-11 08:30:39 +0100

    Remove print.

--- data/main.lua
+++ data/main.lua
@@ -3,20 +3,21 @@
 -- See the Lua API! http://www.solarus-games.org/solarus/documentation/
 
 local game_manager = require("scripts/game_manager")
-
 local solarus_logo = require("scripts/menus/solarus_logo")
 local language_menu = require("scripts/menus/language")
   
+-- This function is called when Solarus starts.
 function sol.main:on_started()
-  -- This function is called when Solarus starts.
-  print("This is a sample quest for Solarus.")
 
   -- Show the Solarus logo initially.
   sol.menu.start(self, solarus_logo)
+
+  -- Show the language selection menu.
   solarus_logo.on_finished = function()
     sol.menu.start(self, language_menu)
   end
 
+  -- Start a gane,
   language_menu.on_finished = function()
     local game = game_manager:create("savegame_file_name")
     game:start()
